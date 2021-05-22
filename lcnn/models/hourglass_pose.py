76ed0a1351c50e48db8af1aa13b8489f04eddf06
menon92 <mehadihasan80@gmail.com>
2021-05-22 19:10:48 +0600

    add code for HourglassPose

--- lcnn/models/hourglass_pose.py
+++ lcnn/models/hourglass_pose.py
@@ -43,6 +43,8 @@ class Bottleneck2D(tf.keras.Model):
         if self.downsample:
             residual = self.downsample(x)
 
+        print("out", out)
+        print('residual', residual)
         out = self.add([out, residual])
 
         return out
@@ -51,3 +53,95 @@ class Bottleneck2D(tf.keras.Model):
         '''Use only for print model summary'''
         x = layers.Input(shape=(16, 16, 8*self.expansion))
         return Model(inputs=[x], outputs=self.call(x))
+
+
+def bottleneck2d(x, filters, downsample=None):
+    residual = x
+
+    if downsample:
+        residual = downsample(x)
+
+    x = layers.BatchNormalization(axis=-1)(x)
+    x = layers.Activation('relu')(x)
+    x = layers.Conv2D(filters, kernel_size=1, padding='SAME', activation='relu')(x)
+    
+    x = layers.BatchNormalization(axis=-1)(x)
+    x = layers.Activation('relu')(x)
+    x = layers.Conv2D(filters, kernel_size=3, padding='SAME', activation='relu')(x)
+    
+    x = layers.BatchNormalization(axis=-1)(x)
+    x = layers.Activation('relu')(x)
+    x = layers.Conv2D(filters*2, kernel_size=1, padding='SAME', activation='relu')(x)
+    
+    x = layers.Add()([x, residual])
+
+    return x
+
+
+class HourglassNet(tf.keras.Model):
+    """Hourglass model from Newell et al ECCV 2016"""
+
+    def __init__(self, block: Bottleneck2D):
+        super(HourglassNet, self).__init__()
+        
+        self.conv1 = layers.Conv2D(64, kernel_size=7, padding='SAME', strides=2)
+        self.bn1 = layers.BatchNormalization()
+        self.relu1 = layers.Activation('relu')
+
+        # layer1 block
+        self.downsample1 = layers.Conv2D(128, kernel_size=1, strides=1)
+        self.layer1 = block(filters=64, downsample=self.downsample1)
+
+        self.max_pool = layers.MaxPooling2D(pool_size=2, strides=2)
+
+        # layer2 block
+        self.downsample2 = layers.Conv2D(256, kernel_size=1, strides=1)
+        self.layer2 = block(filters=128, downsample=self.downsample2)
+
+        # layer3 block, no downsample
+        self.layer3 = block(filters=128)
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+
+        x = self.layer1(x)
+        x = self.max_pool(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        return x
+
+    def model(self):
+        x = layers.Input(shape=(128, 128, 3))
+        return Model(inputs=[x], outputs=self.call(x))
+
+
+def hourglass_net():
+    input_ = layers.Input(shape=(128, 128, 3))
+
+    x = layers.Conv2D(64, kernel_size=7, padding='SAME', strides=2)(input_)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+
+    # block 1
+    x = bottleneck2d(
+        x,
+        filters=64,
+        downsample=layers.Conv2D(128, kernel_size=1, strides=1, name='downsample_1')
+    )
+
+    x = layers.MaxPooling2D(pool_size=2, strides=2)(x)
+
+    # block 2
+    x = bottleneck2d(
+        x,
+        filters=128,
+        downsample=layers.Conv2D(256, kernel_size=1, strides=1, name='downsample_2')
+    )
+
+    # block 3
+    x = bottleneck2d(x, filters=128)
+
+    return  Model(inputs=input_, outputs=x)
--- test/test_hourglass_pose.py
+++ test/test_hourglass_pose.py
@@ -1,13 +1,30 @@
+import tensorflow as tf
+
 from tensorflow.keras import layers
 from tensorflow.keras.models import Model
 from tensorflow.keras.utils import plot_model
 
 from lcnn.models.hourglass_pose import Bottleneck2D
+from lcnn.models.hourglass_pose import HourglassNet
+from lcnn.models.hourglass_pose import hourglass_net
 
 
-filters = 8
-bottleneck2d = Bottleneck2D(filters)
-bottleneck2d = bottleneck2d.model()
-bottleneck2d.summary()
+# filters = 8
+# bottleneck2d = Bottleneck2D(filters)
+# bottleneck2d = bottleneck2d.model()
+# bottleneck2d.summary()
 
-plot_model(bottleneck2d, 'figs/Bottleneck2D.png')
+# plot_model(bottleneck2d, 'figs/Bottleneck2D.png')
+
+# print('-' * 50)
+# hourglass_net = HourglassNet(Bottleneck2D)
+# # hourglass_net = hourglass_net.model()
+# x = tf.random.uniform(shape=(1, 128, 128, 3))
+# hourglass_net(x)
+# hourglass_net.summary()
+
+# plot_model(hourglass_net, 'figs/HourglassNet.png')
+
+model = hourglass_net()
+model.summary()
+plot_model(model, 'figs/HourglassNet-Static-Graph.png')
